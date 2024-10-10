ea9b3a8c491fc1d381ba182c5f85f07d2811b909
sohyun022 <sohyun22@ajou.ac.kr>
2024-10-10 23:01:57 +0900

    Feat: ViewRepository findAllByMember 메서드 추가

--- src/main/java/org/ojt/board_ojt/api/board/repository/ViewRepository.java
+++ src/main/java/org/ojt/board_ojt/api/board/repository/ViewRepository.java
@@ -4,9 +4,16 @@ import org.ojt.board_ojt.api.board.domain.Post;
 import org.ojt.board_ojt.api.board.domain.View;
 import org.ojt.board_ojt.api.member.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ViewRepository extends JpaRepository<View, Long> {
     boolean existsByPostAndMember(Post post, Member member);
+
+    @Query("SELECT v from View v join  fetch v.post p where v.member.memberId = : memberId")
+    List<View> findAllByMember(Long memberId);
+
 }
