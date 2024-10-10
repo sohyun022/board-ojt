da98735e9414d75f51621ae0de3a71a5d4441efe
sohyun022 <sohyun22@ajou.ac.kr>
2024-10-11 00:54:51 +0900

    Refactor: 게시글 좋아요 기능 관련 변경
    Like 엔티티 수정(Member,Post 와의 연관관계 제거)
    생성자 추가

--- src/main/java/org/ojt/board_ojt/api/board/domain/Like.java
+++ src/main/java/org/ojt/board_ojt/api/board/domain/Like.java
@@ -2,24 +2,25 @@ package org.ojt.board_ojt.api.board.domain;
 
 import jakarta.persistence.*;
 import lombok.Getter;
-import org.ojt.board_ojt.api.member.domain.Member;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
 @Table(name = "post_like")
+@NoArgsConstructor
 public class Like {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long likeId; // 좋아요 id
 
-    @ManyToOne
-    @JoinColumn(name = "post_id", nullable = false)
-    private Post postId;  // 좋아요가 눌린 게시글
+    private Long postId;  // 좋아요 한 게시글
 
-    @ManyToOne
-    @JoinColumn(name = "member_id", nullable = false)
-    private Member memberId;  // 좋아요를 누른 사용자
+    private Long memberId;  // 좋아요 한 사용자
 
+    public Like(Long postId, Long memberId) {
+        this.postId = postId;
+        this.memberId = memberId;
+    }
 
 }
