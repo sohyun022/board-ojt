1963e908978e8417fc6c5d2f1d5eb083c47dfb74
sohyun022 <sohyun22@ajou.ac.kr>
2024-10-30 23:10:58 +0900

    remove: Comment 관련 클래스 삭제

--- src/main/java/org/ojt/board_ojt/api/comment/repository/CommentRepository.java
+++
@@ -1,7 +0,0 @@
-package org.ojt.board_ojt.api.comment.repository;
-
-import org.ojt.board_ojt.api.comment.domain.Comment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CommentRepository extends JpaRepository<Comment, Long> {
-}
