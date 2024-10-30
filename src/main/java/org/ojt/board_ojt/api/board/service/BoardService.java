675b88c7ea8856f1f71d3a50fb5c5026681179cf
sohyun022 <sohyun22@ajou.ac.kr>
2024-10-30 23:01:01 +0900

    style: BoardService 인터페이스 메서드 순서 변경

--- src/main/java/org/ojt/board_ojt/api/board/service/BoardService.java
+++ src/main/java/org/ojt/board_ojt/api/board/service/BoardService.java
@@ -13,12 +13,16 @@ import java.util.List;
 
 public interface BoardService {
 
-    //게시글 생성,수정,삭제,복구
+    //게시글 생성,수정,삭제
     Post createPost(CreatePostReq createPostReq, CustomUserDetails userDetails); //게시글 생성
     Post updatePost(UpdatePostReq updatePostReq, Long postId, CustomUserDetails userDetails); //게시글 수정
+    boolean deletePost(Long PostId, CustomUserDetails userDetails);
+
+    //게시글 조회
     List<PostListRes> getPostList(PostListReq postListReq); //게시판 목록 조회
     PostDetailRes getPostDetail(Long PostId, CustomUserDetails userDetails); //게시판 상세 조회
-    boolean deletePost(Long PostId, CustomUserDetails userDetails);
+
+    //게시글 좋아요
     void likePost(Long PostId, CustomUserDetails userDetails);
     void unlikePost(Long PostId, CustomUserDetails userDetails);
 
