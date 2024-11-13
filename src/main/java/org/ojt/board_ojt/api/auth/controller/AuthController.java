bfbcd34ce88ca2520a2bfd9f44afbfe136b24bfa
Sohyun Moon <sohyun22@ajou.ac.kr>
2024-11-13 14:08:46 +0900

    style: Adjust spacing and line breaks for readability

--- src/main/java/org/ojt/board_ojt/api/auth/controller/AuthController.java
+++ src/main/java/org/ojt/board_ojt/api/auth/controller/AuthController.java
@@ -19,27 +19,17 @@ public class AuthController {
 
     @PostMapping("/login")
     public ResponseEntity<?> login(@RequestBody LoginReq loginReq) {
-
         TokenRes tokenRes = authService.login(loginReq);
-
         return ResponseEntity.ok(tokenRes);
     }
 
     @PostMapping("/refresh-token")
     public ResponseEntity<String> refreshAccessToken(@RequestHeader("Authorization") String authorizationHeader) {
-        try {
-            // "Bearer " 접두사를 제거하고 토큰만 추출
-            String refreshToken = authorizationHeader.replace("Bearer ", "");
-            // 액세스 토큰 재발급 시도
-            String newAccessToken = authService.reissuedAccessToken(refreshToken);
-            return ResponseEntity.ok(newAccessToken); // 성공 시 200 OK 응답
-        } catch (JwtException e) {
-            // JWT 관련 예외 처리 (토큰 만료, 유효하지 않은 토큰 등)
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(e.getMessage()); // 401 Unauthorized
-        } catch (Exception e) {
-            // 일반적인 예외 처리
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("서버 오류가 발생했습니다."); // 500 Internal Server Error
-        }
+        // "Bearer " 접두사를 제거하고 토큰만 추출
+        String refreshToken = authorizationHeader.replace("Bearer ", "");
+        // 액세스 토큰 재발급 시도
+        String newAccessToken = authService.reissuedAccessToken(refreshToken);
+        return ResponseEntity.ok(newAccessToken); // 성공 시 200 OK 응답
     }
 
 
