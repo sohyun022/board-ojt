2ae2a38e2e83b8da5e9d0f9580bff2a801d59419
Sohyun Moon <sohyun22@ajou.ac.kr>
2024-11-26 02:07:24 +0900

    feat: Add update query for comments
    -CommentRepository에 댓글 수정을 위한 @Query와 @Modifying 추가

--- src/main/java/org/ojt/board_ojt/api/board/repository/CommentRepository.java
+++ src/main/java/org/ojt/board_ojt/api/board/repository/CommentRepository.java
@@ -2,11 +2,18 @@ package org.ojt.board_ojt.api.board.repository;
 
 import org.ojt.board_ojt.api.board.domain.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
-import java.util.List;
+import java.time.LocalDateTime;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 
     Comment findByCommentId(Long commentId);
+
+    @Modifying
+    @Query("UPDATE Comment e SET e.content = :content, e.updatedAt = :updatedAt WHERE e.commentId = :id")
+    void updateComment(@Param("id") Long id, @Param("content") String content, @Param("updatedAt") LocalDateTime updatedAt);
 
 }
